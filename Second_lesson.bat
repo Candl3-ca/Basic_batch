ea79274ace1a089aad362704b2c908c38a395e71
Candl3-ca <75958530+Candl3-ca@users.noreply.github.com>
2022-10-04 11:25:04 -0400

    Add files via upload

---
+++ Second_lesson.bat
@@ -0,0 +1,43 @@
+@echo off
+color 0A
+title My second batch
+:: start http://www.google.ca this starts the website
+
+:: the easiest way to execute a file through .bat is to have it in the same location/folder
+
+:: start text.txt
+
+
+:: if it's not in the same folder you need to put in the location
+
+:: start C:\Users\....\Desktop\text.txt
+
+echo Hello!
+pause 
+color 05
+echo Hi!
+pause
+color FE
+echo among us?
+pause
+
+:: 0 = Black
+:: 1 = Blue
+:: 2 = Green
+:: 3 = Aqua
+:: 4 = Red
+:: 5 = Purple
+:: 6 = Yellow
+:: 7 = White
+:: 8 = Gray
+:: 9 = Light Blue
+:: A = Light Green
+:: B = Light Aqua
+:: C = Light Red
+:: D = Light Purple
+:: E = Light Yellow
+:: F = Bright White
+
+ 
+
+
