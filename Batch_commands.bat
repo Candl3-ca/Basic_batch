05fe36143322512e7cff6bb110b2407b6980d101
Candl3-ca <75958530+Candl3-ca@users.noreply.github.com>
2022-10-11 12:28:15 -0400

    Update Batch_commands.bat

--- Batch_commands.bat
+++ Batch_commands.bat
@@ -4,14 +4,17 @@ title Directors, Delete command, and Rename command
 
 :: directors are > they lead
 
-echo Hello!>Hello.txt  :: usually saves it in the directory where the batch file is
+echo Hello!>Hello.txt  
+:: usually saves it in the directory where the batch file is
 
 :: > one director override existing files
 :: >> add onto
 
-del C:\Users\Candl3\Desktop\Hello.txt  :: delete then file path
+del C:\Users\Candl3\Desktop\Hello.txt  
+:: delete then file path
 
-ren C:\Users\Candl3\Desktop\Hello.txt Hi.txt :: specify file path then the new name with the extension
+ren C:\Users\Candl3\Desktop\Hello.txt Hi.txt 
+:: specify file path then the new name with the extension
 
 
 
