d176a5068eef9ab22c79323b2bda3559d0243029
Candl3-ca <75958530+Candl3-ca@users.noreply.github.com>
2022-10-11 12:10:14 -0400

    Update User_input.bat

--- User_input.bat
+++ User_input.bat
@@ -5,7 +5,7 @@ title User Input
 echo Press 1 to make me say Hi
 echo Press 2 to make me say bye
 
-set /p choice= 
+set /p choice=    :: /p is the user input command, variable has = sign stuck to it
 
 if %choice% == 1 goto hi
 if %choice% == 2 goto bye
