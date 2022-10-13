f3a5ad5872a866fc5fd976827c4134ef52981f0e
Candl3-ca <75958530+Candl3-ca@users.noreply.github.com>
2022-10-13 16:37:11 -0400

    Create custom_pause.bat

---
+++ custom_pause.bat
@@ -0,0 +1,27 @@
+@echo off
+
+title Custom Pause
+
+echo Loading 
+
+ping localhost -n 2>nul
+
+cls
+
+echo Loading.
+
+ping localhost -n 2>nul
+
+echo Loading..
+
+ping localhost -n 2>nul
+
+echo Loading...
+
+ping localhost -n 2>nul
+
+cls
+
+echo Done laoding
+
+pause
